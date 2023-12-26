ffbe928e49375ff15a11e102d0997ba2f4e5546b
unknown <Yash Yadav>
2023-12-26 15:14:20 +0530

    PersonalWebsite

--- app/page.js
+++ app/page.js
@@ -3,7 +3,12 @@ import styles from './page.module.css'
 export default function Home() {
   return <>
     <div className={styles.mainDiv}>
-
+      <div className={styles.heroSection}>
+        <div className={styles.left}>
+          <h1>Hi, I'm Yash</h1>
+        </div>
+        <div className={styles.right}></div>
+      </div>
     </div>
   </>
 }
